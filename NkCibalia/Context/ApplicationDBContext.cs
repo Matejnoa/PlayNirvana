a10f7e356c0acb330340fa0e5ee9c63e9cf52880
Magdalena <Magdalena@LAPTOP-S12VL7K9>
2023-04-08 17:29:56 +0200

    Add project files.

---
+++ NkCibalia/Context/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NkCibalia.Models;
+
+namespace NkCibalia.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
+        {
+        
+        }
+
+
+        public DbSet<Players> Players { get; set; }
+    }
+}
---
+++ NkCibalia/Models/Players.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NkCibalia.Models
+{
+    public class Players
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+        public string Position { get; set; } = null!;
+        public int Age { get; set; } 
+    }
+}
